ca74de82630a40716f4d8196403c2b935dc43f51
embsignab <embsign@berglund.mx>
2025-08-06 20:04:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,5 +13,5 @@ dig "$TARGET_HOST" || echo "dig failed"
 echo "🔍 DNS lookup with getent:"
 getent hosts "$TARGET_HOST" || echo "getent failed"
 
-echo "Starting simpleproxy on port $PROXY_PORT forwarding to minecraft-server:$TARGET_PORT"
-simpleproxy -v -L "$PROXY_PORT" -R minecraft-server:$TARGET_PORT
+echo "Starting simpleproxy on port $PROXY_PORT forwarding to $TARGET_HOST:$TARGET_PORT"
+simpleproxy -v -L "$PROXY_PORT" -R [$TARGET_HOST]:$TARGET_PORT
