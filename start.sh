f7ce5ebe7e6a6b246527bfd3b8353dc11a6e333f
embsignab <embsign@berglund.mx>
2025-08-06 19:59:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ apt update
 echo "Installing simpleproxy..."
 apt install -y simpleproxy
 
-echo "Starting simpleproxy on port $PROXY_PORT forwarding to $TARGET_HOST:$TARGET_PORT"
+echo "Starting simpleproxy on port $PROXY_PORT forwarding to minecraft-server:$TARGET_PORT"
 simpleproxy -v -L "$PROXY_PORT" -R minecraft-server:$TARGET_PORT
